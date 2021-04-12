112d7396b1d0be873e4498af2713eaad9c1d7810
Tyler <Tyler.Pilkington@mscoding.org>
2021-04-12 12:22:39 -0500

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace postfixCalculator {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            string answer = EvalutatePostFix().ToString();
+            TxtAnswer.Text = answer;
+        }
+
+        private double EvalutatePostFix() {
+            lstOperation.Items.Clear();
+            Stack postFixStack = new Stack();
+            postFixStack.Clear();
+            string expression = TxtNumber1.Text;
+            string tempnum = " ";
+            double tempAnswer = 0;
+            string tempString = " ";
+            
+
+            foreach (char token in expression) {
+                if (token == ' '){
+                    if(tempnum == "") {
+
+                    } else {
+                        postFixStack.Push(tempnum);
+                        tempString = "After push operation: ";
+                        tempString += postFixStack.ToString();
+                        lstOperation.Items.Add(tempString);
+                        tempnum = " ";
+                    }
+                    
+                } else if(token == '+' || token == '-' || token == '/' || token == '*') {
+                    if(token == '+') {
+                        double num1 = double.Parse(postFixStack.Pop().ToString());
+                        double num2 = double.Parse(postFixStack.Pop().ToString());
+                        tempAnswer = num2 + num1;
+                        tempString = "After + operation: ";
+                    } else if(token == '-') {
+                        double num1 = double.Parse(postFixStack.Pop().ToString());
+                        double num2 = double.Parse(postFixStack.Pop().ToString());
+                        tempAnswer = num2 - num1;
+                        tempString = "After - operation: ";
+                    } else if(token == '/') {
+                        double num1 = double.Parse(postFixStack.Pop().ToString());
+                        double num2 = double.Parse(postFixStack.Pop().ToString());
+                        tempAnswer = num2 / num1;
+                        tempString = "After / operation: ";
+                    } else if (token == '*') {
+                        double num1 = double.Parse(postFixStack.Pop().ToString());
+                        double num2 = double.Parse(postFixStack.Pop().ToString());
+                        tempAnswer = num2 * num1;
+                        tempString = "After * operation: ";
+                    }
+                    tempnum = tempAnswer.ToString();
+                    postFixStack.Push(tempnum);
+                    tempnum = "";
+                    tempString += postFixStack.ToString();
+                    lstOperation.Items.Add(tempString);
+                } else tempnum += token;
+            }
+
+            double answer = double.Parse(postFixStack.Pop().ToString());
+
+            return answer;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+    }
+}
---
+++ stack/Stack.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace postfixCalculator {
+    class Stack {
+
+        private class Node {
+            //field
+            private object data;
+            private Node next;
+
+            //properties
+            public object Data {
+                get { return data; }
+                set { data = value; }
+            }//end property
+
+            public Node Next {
+                get { return next; }
+                set { next = value; }
+            }//end property
+
+            //CONSTRUCTOR
+            public Node(object newData = null) {
+                data = newData;
+            }//end construcor
+        }
+        //fields
+        private Node _head = null;
+        private int _count = 0;
+        private Node _bottom = null;
+
+        //property
+        public int Count {
+            get { return _count; }
+            private set { _count = value; }
+        }
+
+        #region methods
+        public void Push(object newData) {
+            //put the new data in a node
+            Node newNode = new Node(newData);
+            //increase the count
+            _count += 1;
+
+            //if head is null make the new data the head
+            if (_head == null) {
+                _head = newNode;
+                _bottom = newNode;
+            //else it will push the old head and make the new data the head
+            } else {
+                _head.Next = newNode;
+                _head = newNode;
+            }
+        }
+
+        public object Pop() {
+            Node newNode = _bottom;
+            if (_head == _bottom) {
+                _head = null;
+                _bottom = null;
+                return newNode.Data;
+            } else if (_head != null) {
+                while (newNode.Next != _head) {
+                newNode = newNode.Next;
+                }
+                
+                _head = newNode;
+                newNode = newNode.Next;
+                _head.Next = null;
+                _count -= 1;
+                return newNode.Data;
+            } else {
+                throw new Exception("head was null");
+            }
+
+        }
+
+        public object Peek() {
+            //if the head is not null return the data to the user
+            if (_head != null) return _head.Data;
+            //if head was null you throw a exception
+            throw new Exception("head was null");
+        }
+
+        public void Clear() {
+            //clear the entire stack
+            _head = null;
+            _bottom = null;
+        }
+
+        public override string ToString() {
+            Node newNode = _bottom;
+            string tempString = "";
+            
+            if (_head == _bottom) {
+                tempString = _head.Data.ToString();
+                tempString += " <--- Top";
+                return tempString;
+            } else if (_head != null) {
+                while (newNode != _head) {
+                    tempString += newNode.Data.ToString();
+                    tempString += " ";
+                    newNode = newNode.Next;
+                }
+                tempString += _head.Data.ToString();
+                tempString += " <--- Top";
+                return tempString;
+            } else {
+                throw new Exception("head was null");
+            }
+        }
+        #endregion
+    }
+}
